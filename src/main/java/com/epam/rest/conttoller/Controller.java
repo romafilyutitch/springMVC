7a7c57fdd0b2dbd77d523ffecf13bf99a2abaf8c
Roman Filiyutich <Roman_Filiyutich@epam.com>
2021-10-08 18:34:03 +0300

    dao service finished

--- src/main/java/com/epam/rest/conttoller/Controller.java
+++ src/main/java/com/epam/rest/conttoller/Controller.java
@@ -1,7 +1,6 @@
 package com.epam.rest.conttoller;
 
 import com.epam.rest.model.GiftCertificate;
-import com.epam.rest.service.GiftCertificateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,15 +11,5 @@ import java.util.List;
 @RestController
 @RequestMapping("/")
 public class Controller {
-    private GiftCertificateService service;
 
-    @Autowired
-    public Controller(GiftCertificateService service) {
-        this.service = service;
-    }
-
-    @RequestMapping(method = RequestMethod.GET)
-    public List<GiftCertificate> findAll() {
-        return service.findAll();
-    }
 }
