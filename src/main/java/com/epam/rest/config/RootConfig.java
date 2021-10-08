185bdfd5279e30e229f570294ea38b9294411e09
Roman Filiyutich <Roman_Filiyutich@epam.com>
2021-10-08 13:40:55 +0300

    data source bean configured

--- src/main/java/com/epam/rest/config/RootConfig.java
+++ src/main/java/com/epam/rest/config/RootConfig.java
@@ -2,8 +2,6 @@ package com.epam.rest.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan("com.epam.rest")
