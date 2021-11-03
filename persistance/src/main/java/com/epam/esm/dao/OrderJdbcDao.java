9fa1013448f55d26843b1a55ec4ea560c25fc361
Roman Filiyutich <Roman_Filiyutich@epam.com>
2021-11-03 13:02:40 +0300

    order dao changed to hubernate. There is one problem

--- persistance/src/main/java/com/epam/esm/dao/OrderJdbcDao.java
+++ persistance/src/main/java/com/epam/esm/dao/OrderJdbcDao.java
@@ -2,11 +2,15 @@ package com.epam.esm.dao;
 
 import com.epam.esm.model.Certificate;
 import com.epam.esm.model.Order;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.io.Serializable;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -16,162 +20,106 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class OrderJdbcDao extends AbstractDao<Order> implements OrderDao {
-    private static final String TABLE_NAME = "certificate_order";
-    private static final List<String> COLUMNS = Arrays.asList("cost", "date", "certificate_id");
-    private static final RowMapper<Order> MAPPER = (rs, rowNum) -> {
-        long id = rs.getLong("id");
-        double cost = rs.getDouble("cost");
-        LocalDateTime date = rs.getObject("date", LocalDateTime.class);
-        return new Order(id, cost, date);
-    };
-    private static final String FIND_ALL_USER_ORDERS_SQL = "select id, cost, date, certificate_id from certificate_order where user_id = ?";
-    private static final String FIND_ORDER_BY_CERTIFICATE_ID_SQL = "select id, cost, date, certificate_id from certificate_order where certificate_id = ?";
-    private static final String FIND_USER_ORDERS_PAGE_SQL = "select id, cost, date, certificate_id from certificate_order where user_id = ? limit ?, 5";
-    private static final String COUNT_USER_ORDERS_SQL = "select count(*) from certificate_order where user_id = ?";
-    private static final String SET_USER_ID_TO_ORDER_SQL = "update certificate_order set user_id = ? where id = ?";
-    private static final String COUNT_COLUMN = "count(*)";
-
-    private final CertificateDao certificateDao;
-
+public class OrderJdbcDao implements OrderDao {
     @Autowired
-    public OrderJdbcDao(CertificateDao certificateDao) {
-        super(TABLE_NAME, COLUMNS, MAPPER);
-        this.certificateDao = certificateDao;
-    }
+    private SessionFactory sessionFactory;
 
-    /**
-     * Finds and returns entities on specified page
-     * @param page that need to be finds
-     * @return entities on passed page
-     */
     @Override
     public List<Order> findPage(int page) {
-        List<Order> allOrders = super.findPage(page);
-        allOrders.forEach(this::addCertificateToOrder);
-        return allOrders;
+        Session session = sessionFactory.getCurrentSession();
+        Query<Order> query = session.createQuery("from Order", Order.class);
+        query.setFirstResult(5 * page - 5);
+        query.setMaxResults(5 * page);
+        return query.list();
     }
-    /**
-     * Finds and returns entity that have passed id
-     *
-     * @param id id of entity that need to be found
-     * @return Optional that contains entity if entity with passed id exists
-     * or empty optional otherwise
-     */
+
     @Override
     public Optional<Order> findById(long id) {
-        Optional<Order> optionalOrder = super.findById(id);
-        optionalOrder.ifPresent(this::addCertificateToOrder);
-        return optionalOrder;
+        Session session = sessionFactory.getCurrentSession();
+        Order order = session.find(Order.class, id);
+        return Optional.ofNullable(order);
     }
-    /**
-     * Perform entity save operation. And assigns calculates by database id to saved entity
-     *
-     * @param entity entity that need to be saved
-     * @return saved entity with assigned id
-     */
+
     @Override
     public Order save(Order entity) {
-        Order savedOrder = super.save(entity);
-        Certificate certificate = entity.getCertificate();
-        Certificate savedCertificate = certificateDao.save(certificate);
-        entity.setCertificate(savedCertificate);
-        return findById(savedOrder.getId()).orElseThrow(DaoException::new);
+        Session session = sessionFactory.getCurrentSession();
+        Serializable id = session.save(entity);
+        return session.find(Order.class, id);
     }
-    /**
-     * Finds and returns user orders by user id
-     * @param userId id of user which orders need to be found
-     * @return list of user orders
-     */
+
+    @Override
+    public Order update(Order entity) {
+        Session session = sessionFactory.getCurrentSession();
+        session.update(entity);
+        return session.find(Order.class, entity.getId());
+    }
+
+    @Override
+    public void delete(long id) {
+        Session session = sessionFactory.getCurrentSession();
+        Order order = session.find(Order.class, id);
+        session.delete(order);
+    }
+
+    @Override
+    public int getTotalElements() {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Long> query = session.createQuery("select count(*) from Order", Long.class);
+        Long totalElements = query.uniqueResult();
+        return totalElements.intValue();
+    }
+
+    @Override
+    public int getTotalPages() {
+        int totalElements = getTotalElements();
+        int totalPages = totalElements / 5;
+        return totalElements % 5 == 0 ? totalPages : ++totalPages;
+    }
+
     @Override
     public List<Order> findAllUserOrders(long userId) {
-        List<Order> foundOrders = template.query(FIND_ALL_USER_ORDERS_SQL, MAPPER, userId);
-        foundOrders.forEach(this::addCertificateToOrder);
-        return foundOrders;
+        Session session = sessionFactory.getCurrentSession();
+        Query<Order> query = session.createQuery("from Order where user_id = ?1", Order.class);
+        query.setParameter(1, userId);
+        return query.list();
     }
-    /**
-     * Sets user to saved order.
-     * @param userId id of user that need to be saved to order
-     * @param orderId order id that need to be set to user
-     */
+
     @Override
-    public void setUserToOrder(long userId, long orderId) {
-        template.update(SET_USER_ID_TO_ORDER_SQL, userId, orderId);
+    public List<Order> findUserOrdersPage(long userId, int page) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Order> query = session.createQuery("from Order where user_id = ?1", Order.class);
+        query.setParameter(1, userId);
+        query.setFirstResult(5 * page - 5);
+        query.setMaxResults(5 * page);
+        return query.list();
     }
-    /**
-     * Finds certificate order by passed certificate id
-     * @param certificateId whose order need to be found
-     * @return order that contains certificate with passed id
-     * or empty order otherwise
-     */
-    @Override
-    public Optional<Order> findByCertificateId(long certificateId) {
-        List<Order> orders = template.query(FIND_ORDER_BY_CERTIFICATE_ID_SQL, MAPPER, certificateId);
-        orders.forEach(this::addCertificateToOrder);
-        return orders.isEmpty() ? Optional.empty() : Optional.of(orders.get(0));
-    }
-    /**
-     * Finds and returns passed user passed orders page
-     * @param userId user id which orders need to be found
-     * @param page user orders page that need to be found
-     * @return user orders passed page
-     */
-    @Override
-    public List<Order> findUserOrdersPage(long userId, long page) {
-        List<Order> orders = template.query(FIND_USER_ORDERS_PAGE_SQL, MAPPER, userId, (ROWS_PER_PAGE * page) - ROWS_PER_PAGE);
-        orders.forEach(this::addCertificateToOrder);
-        return orders;
-    }
-    /**
-     * Counts user orders pages amount.
-     * @param userId id of user which orders pages need to be count
-     * @return amount of user orders pages
-     */
-    @Override
-    public int getUserOrdersTotalPages(long userId) {
-        int rows = template.queryForObject(COUNT_USER_ORDERS_SQL, (rs, rowNum) -> rs.getInt(COUNT_COLUMN), userId);
-        int pages = (rows / ROWS_PER_PAGE);
-        return rows % ROWS_PER_PAGE == 0 ? pages : ++pages;
-    }
-    /**
-     * Counts user orders amount.
-     * @param userId id of user which orders amount need to be count
-     * @return amount of user orders
-     */
+
     @Override
     public int getUserOrdersTotalElements(long userId) {
-        return template.queryForObject(COUNT_USER_ORDERS_SQL, (rs, rowNum) -> rs.getInt(COUNT_COLUMN), userId);
+        Session session = sessionFactory.getCurrentSession();
+        Query<Long> query = session.createQuery("select count(*) from Order where user_id = ?1", Long.class);
+        query.setParameter(1, userId);
+        Long userOrdersTotalElements = query.uniqueResult();
+        return userOrdersTotalElements.intValue();
     }
 
-    /**
-     * Maps entity values to Prepared save statement
-     * @param saveStatement PreparedStatement that need to be set entity values for save
-     * @param entity        entity that need to be saved
-     * @throws SQLException if exception in database occurs
-     */
     @Override
-    protected void setSaveValues(PreparedStatement saveStatement, Order entity) throws SQLException {
-        saveStatement.setDouble(1, entity.getCost());
-        saveStatement.setObject(2, entity.getOrderDate());
-        saveStatement.setLong(3, entity.getCertificate().getId());
+    public int getUserOrdersTotalPages(long userId) {
+        int userOrdersTotalElements = getUserOrdersTotalElements(userId);
+        int userOrdersTotalPages = userOrdersTotalElements / 5;
+        return userOrdersTotalElements % 5 == 0 ? userOrdersTotalPages : ++userOrdersTotalPages;
     }
 
-    /**
-     * Maps entity values to Prepared updated statement
-     * @param updateStatement Prepared statement that need to be set entity values for update
-     * @param entity          entity that need to be updated
-     * @throws SQLException if exception in database occurs
-     */
     @Override
-    protected void setUpdateValues(PreparedStatement updateStatement, Order entity) throws SQLException {
-        updateStatement.setDouble(1, entity.getCost());
-        updateStatement.setObject(2, entity.getOrderDate());
-        updateStatement.setLong(3, entity.getCertificate().getId());
-        updateStatement.setLong(4, entity.getId());
+    public void setUserToOrder(long userId, long orderId) {
+//        @todo fix this
     }
 
-    private void addCertificateToOrder(Order order) {
-        Optional<Certificate> optionalCertificate = certificateDao.findByOrderId(order.getId());
-        order.setCertificate(optionalCertificate.orElseThrow(DaoException::new));
+    @Override
+    public Optional<Order> findByCertificateId(long certificateId) {
+        Session session = sessionFactory.getCurrentSession();
+        Query<Order> query = session.createQuery("from Order where certificate.id = ?1", Order.class);
+        query.setParameter(1, certificateId);
+        return query.uniqueResultOptional();
     }
 }
