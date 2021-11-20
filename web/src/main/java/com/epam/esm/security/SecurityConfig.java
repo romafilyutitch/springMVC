540f82fb8202017209eb8dd3255dec74245ef921
Roman Filiyutich <Roman_Filiyutich@epam.com>
2021-11-20 16:42:46 +0300

    authorization filter added

--- web/src/main/java/com/epam/esm/security/SecurityConfig.java
+++ web/src/main/java/com/epam/esm/security/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.epam.esm.security;
 
 import com.epam.esm.security.filter.CustomAuthenticationFilter;
+import com.epam.esm.security.filter.CustomAuthorizationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,8 +10,11 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.POST;
 
 @Configuration
 @EnableWebSecurity
@@ -33,10 +36,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-       http.csrf().disable();
-       http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-       http.authorizeRequests().anyRequest().permitAll();
-       http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeRequests().antMatchers("/login").permitAll();
+        http.authorizeRequests().anyRequest().hasAuthority("ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(GET, "/certificates/**").hasAnyAuthority("ROLE_GUEST", "ROLE_USER", "ROLE_ADMIN");
+        http.authorizeRequests().antMatchers(GET, "/signup").hasAuthority("ROLE_GUEST");
+        http.authorizeRequests().antMatchers(POST, "/signup").hasAuthority("ROLE_GUEST");
+        http.authorizeRequests().anyRequest().authenticated();
+        http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+        http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
 
